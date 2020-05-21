d84bbeecb59282e652fc7275fbf2e2bf31aa3b13
RuslanG2 <grusil@mail.ru>
2020-05-22 00:28:39 +0500

    Внёс переменные и библиотеки

--- ProjectFYGH/src/main.java
+++ ProjectFYGH/src/main.java
@@ -1,4 +1,16 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.io.FileNotFoundException;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.util.*;
 
-public class main {
-
-}
+public class Main extends JFrame {
+	public JLabel tname;
+	static public JTextField login;
+	static public JPasswordField pass;
+	public JButton enter;
+	public JPanel panel;
+	static int col;
+	
